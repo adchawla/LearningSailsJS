ad8c6912732e970b0ef4325bfd6b7e94087ae328
Amandeep Chawla <adchawla@gmail.com>
2017-05-21 12:48:54 +0530

    First sails application with life controller added

---
+++ 001_firstApp/firstApp/api/controllers/Life?Controller.js
@@ -0,0 +1,11 @@
+/**
+ * Life?Controller
+ *
+ * @description :: Server-side logic for managing life?s
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
