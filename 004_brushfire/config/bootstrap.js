9da172ca261356e0cdb55c881fbd67512a85daad
Amandeep Chawla <adchawla@gmail.com>
2017-05-21 16:36:32 +0530

    Getting the list from the youtube api using machine packs

--- 004_brushfire/config/bootstrap.js
+++ 004_brushfire/config/bootstrap.js
@@ -11,7 +11,43 @@
 
 module.exports.bootstrap = function(cb) {
 
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  Video.count().exec(function(err, numVideos){
+    if (err) {
+      return cb(err);
+    }
+    if (numVideos > 0) {
+      console.log('Number of video records: ', numVideos);
+      return cb();
+    }
+    var youTubeMP = require('machinepack-youtube');
+    youTubeMP.searchVideos({
+      query:'grumy cat',
+      apiKey: sails.config.google.apiKey,
+      limit:15
+    }).exec(
+      {
+        error:function(err) {
+          console.log("an error occured: ", err);
+          return cb(err);
+        },
+        success: function(foundVideos) {
+          _.each(foundVideos, function(video){
+            video.src = 'https://www.youtube.com/embed/' + video.id;
+            delete video.id;
+            delete video.url;
+            delete video.description;
+            delete video.publishedAt;
+          });
+          Video.create(foundVideos).exec(function(err, videoRecordsCreated){
+            if(err) {
+              return cb(err);
+            }
+            console.log(videoRecordsCreated);
+            return cb();
+          });
+          console.log(foundVideos);
+        }
+      }
+    );
+  });
 };
