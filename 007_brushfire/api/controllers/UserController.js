1b947fc9ea043a753374733d190b6d9eb1458e1e
Amandeep Chawla <adchawla@gmail.com>
2017-05-23 13:17:28 +0530

    Refactoring code to add a new response handler

--- 007_brushfire/api/controllers/UserController.js
+++ 007_brushfire/api/controllers/UserController.js
@@ -67,15 +67,13 @@ module.exports = {
                     if (err.invalidAttributes && err.invalidAttributes.email &&
                       err.invalidAttributes.email[0] &&
                       err.invalidAttributes.email[0].rule === 'unique') {
-
-                      return res.send(409, 'Email address is already taken by another user, please try again.');
+                        return res.alreadyInUse(err);
                     }
 
                     if (err.invalidAttributes && err.invalidAttributes.username &&
                       err.invalidAttributes.username[0] &&
                       err.invalidAttributes.username[0].rule === 'unique') {
-
-                      return res.send(409, 'Username is already taken by another user, please try again.');
+                        return res.alreadyInUse(err);
                     }
 
                     return res.negotiate(err);
@@ -90,7 +88,5 @@ module.exports = {
         });
       },
     });
-
-
   }
 };
---
+++ 007_brushfire/api/responses/alreadyInUse.js
@@ -0,0 +1,14 @@
+module.exports = function alreadyInUse(err) {
+    // Get access to the respone object
+    var res = this.res;
+
+    if (err.invalidAttributes.email) {
+        return res.send(409, 'Email address is already taken by another user, please try again');
+    }
+
+    if (err.invalidAttributes.username) {
+        return res.send(409, 'Username is already taken by another user, please try again');
+    }
+
+    return res.send(500);
+};
